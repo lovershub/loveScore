08fa6526ac3d6c59ccacc30cf053f36ecb2da097
lovershub <159955122+lovershub@users.noreply.github.com>
2024-02-14 22:13:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ calcBtn.addEventListener("click", function (e) {
             }
   
 
-  fetch("https://formspree.io/f/xrgnqqzn", {
+  fetch("https://formspree.io/f/xwkgooja", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
